7690ba5ee97ab758c397aebe96b0912a7f9104f0
Tapowan Auto-Sync <admin@tapowanschool.com>
2026-07-15 21:43:36 +0530

    Auto-update Tunnel URL (Serveo)

--- api-config.js
+++ api-config.js
@@ -1,2 +1,2 @@
 // Auto-updated by Tapowan Server
-window.API_BASE_URL = "https://25c6219b009a8402-117-214-76-102.serveousercontent.com";
+window.API_BASE_URL = "https://4317404c872d1ff1-117-214-76-102.serveousercontent.com";
